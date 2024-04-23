d0e2be67148d1909c3e4840356f03a0e9fe15890
jeangathoni8 <jeangathoni39@gmail.com>
2024-04-23 12:18:12 +0300

    final changes

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,25 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import RockyMountain from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+function ColoradoStateParks() {
+  MesaVerde(); // => "Mesa Verde National Park"
+
+  return <h1>Colorado State Parks!</h1>;
+}
+
+function ColoradoStateParks() {
+  RockyMountain() // => "Rocky Mountain National Park"
+
+  return <h1>Colorado State Parks!</h1>;
+}
+
+export default ColoradoStateParks;
